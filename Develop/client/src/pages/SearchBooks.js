26ee470d7b2cc74dd84422c2a9bfb0219f17bb16
slawless08 <samuel.l.lawless@gmail.com>
2021-10-11 17:04:53 -0400

    Updated searchbooks.js to use graphql mutations.

--- Develop/client/src/pages/SearchBooks.js
+++ Develop/client/src/pages/SearchBooks.js
@@ -5,6 +5,9 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
+import { useMutation } from "@apollo/client";
+import { saveBook } from '../utils/mutations';
+
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
@@ -65,7 +68,15 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: {
+          saveBookDescription: bookToSave.description,
+          saveBookId: bookToSave.bookId,
+          saveBookImage: bookToSave.image,
+          saveBookTitle: bookToSave.title,
+          saveBookAuthors: bookToSave.authors
+        }
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
